0193310b66b2f05db23828a7ba21a854196c8d86
Linh Nguyen D.Q <linh.nguyen2@asiantech.vn>
2017-12-11 10:26:18 +0700

    fixed spaces

--- JS_Exercise/cau2.js
+++ JS_Exercise/cau2.js
@@ -1,13 +1,13 @@
 function Compare(n) {
-	if(n === 9) {
-		return 0;
-	}
-	else if(n < 19) {
-		return 19 - n;
-	}
-	else {
-		return 3 * (n - 19);
-	}
+  if(n === 9) {
+    return 0;
+  }
+  else if(n < 19) {
+    return 19 - n;
+  }
+  else {
+    return 3 * (n - 19);
+  }
 }
 
 console.log(Compare(12));
