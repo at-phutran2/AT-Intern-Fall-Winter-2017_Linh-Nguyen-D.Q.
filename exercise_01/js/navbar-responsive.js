98e636a232faac110d6c825ee7b495e4703772af
Linh Nguyen D.Q <linh.nguyen2@asiantech.vn>
2017-12-18 17:31:54 +0700

    fixed

--- exercise_01/js/navbar-responsive.js
+++ exercise_01/js/navbar-responsive.js
@@ -1,8 +1,9 @@
-function myFunction() {
-    var x = document.getElementById("my-mm");
-    if (x.className === "main-menu") {
-        x.className += " responsive";
-    } else {
-        x.className = "main-menu";
-    }
+document.getElementsByClassName('js-toggle-menu')[0].addEventListener('click', function(){
+  toggleMenu();
+});
+
+
+function toggleMenu() {
+  var mm = document.getElementById('my-mm');
+  mm.className === 'main-menu' ? mm.className += ' active' : mm.className = 'main-menu';
 }
