4e1b32a1962ba8d0bcd42153935c2d64b7081a69
Linh Nguyen D.Q <linh.nguyen2@asiantech.vn>
2017-12-18 16:36:21 +0700

    fixed and add delete button

--- JS_Exercise/day_04/js/test.js
+++ JS_Exercise/day_04/js/test.js
@@ -1,3 +1,13 @@
+let count = 0;
+let $noti = document.getElementById('noti');
+let $buy = document.getElementsByClassName('buy-btn');
+let tb = document.getElementsByTagName('table')[0];
+$noti.innerHTML = count;
+let items = [];
+let item;
+let prdlength = $buy.length;
+
+//Show data table
 document.getElementById('cart-btn').addEventListener('click', function() {
   let x = document.getElementById('cart-detail');
   if(x.className === 'hidden') {
@@ -7,42 +17,96 @@ document.getElementById('cart-btn').addEventListener('click', function() {
   }
 });
 
-let count = 0;
-let $noti = document.getElementById('noti');
-let $buy = document.getElementsByClassName('buy-btn');
-$noti.innerHTML = count;
-let items = [];
-let item;
-
-for(let i = 0; i < 3; i++) {
-  item = {
-    index: i,
-    name: $buy[i].getAttribute("data-name"),
-    price: $buy[i].getAttribute("data-price"),
-    quantity: 0,
-    total: 0,
-  }
-  items.push(item);
+//Add item to items
+for(let i = 0; i < prdlength; i++) {
   $buy[i].addEventListener('click', function() {
     count++;
     $noti.innerHTML = count;
-    items[i].quantity++;
-    console.log('Index: ' + items[i].index + '; quantity: ' + items[i].quantity);
-    refreshDT();
+    item = {
+      index: $buy[i].getAttribute('data-id'),
+      name: $buy[i].getAttribute('data-name'),
+      price: $buy[i].getAttribute('data-price'),
+      quantity: 1,
+      total: 0,
+    }
+    
+    let itslength = items.length;
+    let checkExist = false;
+    for(let j = 0; j < itslength; j++){
+      if(item.index === items[j].index){
+        items[j].quantity++;
+        checkExist = true;
+        break;
+      }
+    }
+    if(checkExist == false){
+      items.push(item);
+      createTable(item.index);
+    }
+
+    refreshData();
   });
 }
 
 
-function refreshDT() {
-  for(let i = 0; i < 3; i++) {
-    items[i].total = items[i].quantity * items[i].price;
-    let j = 0;
-    let $tds = document.getElementsByClassName('tb-prd')[i].children;
-    for (var attr in items[i]) {
-      $tds[j].innerHTML = items[i][attr];
-      j++;
+//Create table and remove
+function createTable(id) {
+  let tr = document.createElement('tr');  
+  tr.className = 'tb-prd';
+  tr.setAttribute('data-id', id);
+  document.getElementsByTagName('tbody')[0].appendChild(tr);
+  for(let i = 0; i < 6; i++) {
+    let td = document.createElement('td');
+    let t = document.createTextNode('Delete');
+    let act = document.createElement('button');
+    act.appendChild(t);
+    act.className = 'btn btn-danger';
+    act.setAttribute('data-id', id);
+    if(i === 5) {
+      td.appendChild(act);
     }
-  }  
+    else {
+      td.appendChild(t);
+    }
+    tr.appendChild(td);
+
+    act.addEventListener('click', function() {
+      for(let j = 0; j < items.length; j++) {
+        if(items[j].index === act.getAttribute('data-id')) {
+          let r = confirm("Are you sure to delete?");
+          if (r == true) {
+            items.splice(j, 1);
+            count = count - items[j].quantity;
+            $noti.innerHTML = count;
+            tr.parentNode.removeChild(tr);
+            alert('Deleted success!');
+          }
+          refreshData();
+        }
+      }
+    });
+  }
 }
 
-refreshDT();
+function refreshData() {
+  let empty = document.getElementsByTagName('h3')[0];
+  if(count === 0) {
+    tb.className = 'hidden';
+    empty.className = 'noti-in-card';
+  }
+  else {
+    tb.className = 'table';
+    empty.className = 'hidden';
+    for(let i = 0; i < items.length; i++) {
+      items[i].total = items[i].quantity * items[i].price;
+      let j = 0;
+      let $tds = document.getElementsByClassName('tb-prd')[i].children;
+      for (var attr in items[i]) {
+        $tds[j].innerHTML = items[i][attr];
+        j++;
+      }
+    }
+  }
+}
+
+refreshData();
